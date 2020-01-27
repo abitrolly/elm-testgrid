e56a17cafba478cfe46805569908ecd28e22a106
Anatoli Babenia <anatoli@rainforce.org>
2020-01-27 22:49:22 +0300

    <button> to rule 'em all

--- src/Main.elm
+++ src/Main.elm
@@ -4,11 +4,22 @@
 
 module Main exposing (..)
 
+import Html exposing (div, button, text)
+import Html.Attributes exposing (style)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 
 main =
+  div [] [
+  button [ Html.Attributes.style "height" "20px",
+           Html.Attributes.style "padding" "0",
+           -- trying to keep text label inside button
+           Html.Attributes.style "line-height" "0",
+           -- place button at the top of the parent div
+           Html.Attributes.style "vertical-align" "top"
+         ]
+         [ Html.text ">>" ],
   svg
     [ width "400", height "100" ]
     [ rect [ width "10", height "10" ] []
@@ -19,4 +30,4 @@ main =
     , rect [ x "50", y "10", width "10", height "10" ] []
     , rect [ x "60", y "10", width "10", height "10", fill "red" ] []
     , rect [ x "70", y "10", width "10", height "10", fill "green" ] []
-    ]
+    ]]
